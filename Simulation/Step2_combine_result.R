c2e48f79798b1ff4e438e6a958557b922ec0fb18
qinnnnnf <98192923+qinnnnnf@users.noreply.github.com>
2025-06-21 02:58:57 +1000

    Delete Simulation directory

--- Simulation/Step2_combine_result.R
+++
@@ -1,5 +0,0 @@
-tag <- "ix1"
-files <- list.files(pattern = paste0("^out_", tag, "_.*\\.rds$"))
-all_out <- lapply(files, readRDS)
-out_combined <- do.call(c, all_out)
-saveRDS(out_combined, file = paste0("out_all_", tag, ".rds"))
